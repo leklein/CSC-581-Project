7cb1823a2a70ca48878a74197bd43fa350d449b4
iris <ikohler@calpoly.edu>
2019-05-13 17:55:30 -0700

    Renamed to Predicate (whoops)

---
+++ Predicate.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Predicate {
+   String name;
+   List<String> variables;
+
+   public Predicate(String name, List<String> variables) {
+      this.name = name;
+      this.variables = variables;
+   }
+
+   public boolean equals(Object other) {
+      if (other == null || !other.getClass().equals(getClass())) {
+         return false;
+      }
+      Predicate o = (Predicate)other;
+      return (o.name.equals(name) &
+              o.variables.equals(variables));
+   }
+}
