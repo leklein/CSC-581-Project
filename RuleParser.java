94950e0e062ebe76023b64fb4654dd5c6dd8cced
Namjoon <eric_joo@ymail.com>
2019-05-15 16:58:05 -0700

    Additional implementation for parsing rules.

--- RuleParser.java
+++ RuleParser.java
@@ -5,52 +5,58 @@ import java.util.LinkedList;
 
 public class RuleParser {
 
-    public RuleParser() { }
+    public static void main(String[] args) {
+        RuleParser parser = new RuleParser();
+        List<Rule> rules = parser.generateRules("files/rules.txt");
+        for (Rule rule : rules) {
+            System.out.println("RULE:");
+            for (Predicate pred : rule.predicates) {
+                System.out.print(pred.name + ":");
+                for (String var : pred.variables) {
+                    System.out.print(var + ",");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
     
-    public List<Predicate> readRules(String fileName) {
+    public List<Rule> generateRules(String fileName) {
         Scanner scanner = new Scanner(fileName);
-        List<Predicate> preds = new LinkedList<Predicate>();
+        List<Rule> rules = new LinkedList<Rule>();
 
         String line;
         while (scanner.hasNextLine()) {
             line = scanner.nextLine();
 
-            String pred = "";
-            String[] predArgs = {};
+            List<Predicate> preds = new LinkedList<Predicate>();
+
+            String predName = "";
+            String[] predVars = {};
 
             int predStart = 0;
             int argsStart = 0;
-
-            boolean andConnected = false;
             
             for (int i = 0; i < line.length(); i++)
             {
                 if (line.charAt(i) == ':') {
-                    pred = line.substring(predStart, i);
+                    predName = line.substring(predStart, i);
                     argsStart = i + 1;
                 }
 
-                if (line.charAt(i) == '|' || line.charAt(i) == '&') {
-                    predArgs = line.substring(argsStart, i).split(",");
+                if (line.charAt(i) == '|') {
+                    predVars = line.substring(argsStart, i).split(",");
                     predStart = i + 2;
-                    if (line.charAt(i) == '&') {
-                        andConnected = true;
-                    }
                 }
 
-                if (andConnected) {
-                    // TODO '&' vs '|' distinction
-
-                    andConnected = false;
-                }
-
-                // TODO add all predicates to list? or is '|' end of a rule?
-                preds.add(new Predicate(pred, Arrays.asList(predArgs)));
+                preds.add(new Predicate(predName, Arrays.asList(predVars)));
             }
+
+            rules.add(new Rule(preds));
         }
 
         scanner.close();
 
-        return preds;
+        return rules;
     }
 }
