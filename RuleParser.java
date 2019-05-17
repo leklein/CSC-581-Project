28a97f9b0a25b53dd0b7d813fb38be1049914636
Namjoon <eric_joo@ymail.com>
2019-05-17 12:51:43 -0700

    Bug fixes for rule parsing.

--- RuleParser.java
+++ RuleParser.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.LinkedList;
 
@@ -8,54 +10,64 @@ public class RuleParser {
     public static void main(String[] args) {
         RuleParser parser = new RuleParser();
         List<Rule> rules = parser.generateRules("files/rules.txt");
+
         for (Rule rule : rules) {
             System.out.println("RULE:");
             for (Predicate pred : rule.predicates) {
                 System.out.print(pred.name + ":");
-                for (String var : pred.variables) {
-                    System.out.print(var + ",");
+                for (int i = 0; i < pred.variables.size(); i++) {
+                    System.out.print(pred.variables.get(i));
+                    if (i != pred.variables.size() - 1) {
+                        System.out.print(",");
+                    }
                 }
                 System.out.println();
             }
             System.out.println();
         }
     }
-    
+
     public List<Rule> generateRules(String fileName) {
-        Scanner scanner = new Scanner(fileName);
+
         List<Rule> rules = new LinkedList<Rule>();
 
-        String line;
-        while (scanner.hasNextLine()) {
-            line = scanner.nextLine();
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
 
-            List<Predicate> preds = new LinkedList<Predicate>();
+            String line;
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
 
-            String predName = "";
-            String[] predVars = {};
+                List<Predicate> preds = new LinkedList<Predicate>();
 
-            int predStart = 0;
-            int argsStart = 0;
-            
-            for (int i = 0; i < line.length(); i++)
-            {
-                if (line.charAt(i) == ':') {
-                    predName = line.substring(predStart, i);
-                    argsStart = i + 1;
+                String predName = "";
+                String[] predVars = {};
+
+                int predStart = 0;
+                int argsStart = 0;
+
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == ':') {
+                        predName = line.substring(predStart, i);
+                        argsStart = i + 1;
+                    }
+
+                    if (line.charAt(i) == '|') {
+                        predVars = line.substring(argsStart, i).split(",");
+                        predStart = i + 2;
+                    }
+
+                    preds.add(new Predicate(predName, Arrays.asList(predVars)));
                 }
 
-                if (line.charAt(i) == '|') {
-                    predVars = line.substring(argsStart, i).split(",");
-                    predStart = i + 2;
-                }
-
-                preds.add(new Predicate(predName, Arrays.asList(predVars)));
+                rules.add(new Rule(preds));
             }
 
-            rules.add(new Rule(preds));
-        }
+            scanner.close();
 
-        scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         return rules;
     }
