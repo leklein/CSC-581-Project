7eec6b236058fc5315406d3b95ff4801fb66cf96
lauren.klein <lklein@ctic-inc.com>
2019-06-05 16:40:16 -0700

    ui change

---
+++ src/driver/GameDriverPanel.java
@@ -0,0 +1,32 @@
+/*
+ * Created by JFormDesigner on Wed Jun 05 15:38:26 PDT 2019
+ */
+
+package driver;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author User #5
+ */
+public class GameDriverPanel extends JPanel {
+    public GameDriverPanel() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+        setLayout(new GridBagLayout());
+        ((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0};
+        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0};
+        ((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 1.0E-4};
+        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
